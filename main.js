399dedd47d015c65db8444e0d8bb3b309c27e269
elasri21 <islame.edu@gmail.com>
2022-07-17 20:30:19 +0100

    fixing some issues

--- main.js
+++ main.js
@@ -8,7 +8,30 @@ singUp.addEventListener("click", function(e) {
 
 logIn.addEventListener("click", function(e){
     e.preventDefault();
-    document.body.innerHTML = "<p class='greeting'>Coming Soon!</p>";
-
-
+    document.querySelector(".container").innerHTML = "";
+    createLoginForm(); 
 });
+
+
+function createLoginForm() {
+    let form = document.createElement("form");
+    form.setAttribute("class", "greeting");
+    let emailField = document.createElement("input");
+    setAttributes(emailField, {"type": "email","name": "user-email", "placeholder": "Enter your email"});
+    let passwordField = document.createElement("input");
+    setAttributes(passwordField, {"type": "password","name": "password", "placeholder": "Enter your password"});
+    let login = document.createElement("input");
+    setAttributes(login, {"type": "submit", "value": "login", "class": "login-btn"});
+    form.appendChild(emailField);
+    form.appendChild(passwordField);
+    form.appendChild(login);
+    document.body.appendChild(form);
+}
+
+
+function setAttributes(el, attrs) {
+    for(let key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+}
+
